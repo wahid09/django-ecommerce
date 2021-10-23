b21d14f093ed37b21bd148fe57fa0a3d37d87c49
wahid09 <wahid.ullah.cse@gmail.com>
2021-10-23 11:55:45 +0600

    bug fix for cart

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.manu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from cart.models import CartItem
+from cart.views import _cart_id
 
 # Create your views here.
 
@@ -26,9 +28,11 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug=None, product_slug=None):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
+        'in_cart': in_cart
     }
     return render(request, 'store/product_detail.html', context)
